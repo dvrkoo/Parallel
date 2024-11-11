bbedf1fc614bae2c1c22e055f9e63f5dfe697e2e
dvrko <nico.marini@hotmail.it>
2024-11-11 10:50:14 +0100

    First commit,

---
+++ KMeans/include/KMeans.h
@@ -0,0 +1,11 @@
+
+// MyClass.h
+#ifndef KMEANS_H
+#define KMEANS_H
+
+class KMeans {
+public:
+  void sayHello();
+};
+
+#endif // MYCLASS_H
---
+++ KMeans/src/main.cpp
@@ -0,0 +1,11 @@
+
+// Your First C++ Program
+
+#include <iostream>
+
+int add(int a, int b) { return a + b; }
+int main() {
+  std::cout << "Hello World!";
+
+  return 0;
+}
